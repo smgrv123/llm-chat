319d4a1a1054e488b5a2006c6fa73179d661909a
Sumrit Grover <sumritgrover1@gmail.com>
2024-08-22 21:02:30 +0530

    Cleanup template - remove axios and use fetch - removed redux - removed unnecessary packages

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,4 @@
-import useTemplate from '@/src/hooks/useTemplate';
-
 export default function Home() {
-  const { fetchTemplate } = useTemplate();
-
-  fetchTemplate();
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       hello world
